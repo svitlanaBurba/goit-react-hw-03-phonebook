1ded43f0eb2c5fb36ef85fa3756585ae84bb6258
Svitlana Burba <svitlana.burba@gmail.com>
2021-06-15 01:16:11 +0200

    HW done

---
+++ src/components/filter/Filter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {InputContainer, LabelContainer} from '../contactForm/ContactFormStyled';
+
+const Filter = ({handleChange, filter}) => {
+  return (
+    <>
+      <LabelContainer htmlFor="filter">Find contacts by name</LabelContainer>
+      <InputContainer
+        id="filter"
+        type="text"
+        onChange={handleChange}
+        name="filter"
+        value={filter}
+      />
+    </>
+  );
+};
+
+Filter.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  filter: PropTypes.string.isRequired
+};
+
+export default Filter;
